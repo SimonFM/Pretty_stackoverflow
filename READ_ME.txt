6c3664c633afc0ae5cdfdeffc67d127a2b356371
Simon <markhams@tcd.ie>
2015-07-11 16:54:34 +0100

    Adding my code
    Added the full working extension

---
+++ READ_ME.txt
@@ -0,0 +1,2 @@
+A chrome app to make Stackoverflow easier to read
+	ie removes all that icky whitespace :3
---
+++ changeColour.js
@@ -0,0 +1,31 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({
+	  code: 'changeThings();'
+	  });
+ });
+ 
+chrome.tabs.onUpdated.addListener(function(tab){
+	chrome.tabs.executeScript({
+	  code: 'changeThings();'
+	  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.backgroundColor="#a4a9ab";
+	document.getElementById("content").style.backgroundColor = "#a4a9ab";
+	var all = document.getElementsByClassName("summary");
+	for(var i = 0; i < all.length;i++)
+	{
+		all[i].style.color = "#000000"
+	};
+});
+ 
+function changeThings(){
+	document.body.style.backgroundColor="#a4a9ab";
+	document.getElementById("content").style.backgroundColor = "#a4a9ab";
+	var all = document.getElementsByClassName("summary");
+	for(var i = 0; i < all.length;i++)
+	{
+		all[i].style.color = "#000000"
+	};
+}
