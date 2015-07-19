c3373e32c4902436a3310363d395a6fce29fc16d
Simon <markhams@tcd.ie>
2015-07-19 19:56:00 +0100

    grey's any background
    Also auto for reddit and a few others

--- changeColour.js
+++ changeColour.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-	  code: 'changeThings();'
+	  code: 'document.body.style.backgroundColor="#B6B6B8";document.getElementById("content").style.backgroundColor = "#B6B6B8";document.getElementById("sidebar").style.backgroundColor = "#B6B6B8";var all = document.getElementsByClassName("summary");for(var i = 0; i < all.length;i++){all[i].style.color = "#000000"};document.getElementsByClassName("details").style.backgroundColor = "#B6B6B8";'
 	  });
  });
  
@@ -13,9 +13,11 @@ chrome.tabs.onUpdated.addListener(function(tab){
 document.addEventListener("DOMContentLoaded", changeThings());
  
 function changeThings(){
-	document.body.style.backgroundColor="#a4a9ab";
-	document.getElementById("content").style.backgroundColor = "#a4a9ab";
-	document.getElementById("sidebar").style.backgroundColor = "#a4a9ab";
+	document.body.style.backgroundColor="#B6B6B8";
+	document.getElementById("content").style.backgroundColor = '#B6B6B8';
+	document.getElementById("middleContainer").style.backgroundColor = '#B6B6B8';
+	document.getElementById("sidebar").style.backgroundColor = '#B6B6B8';
+	document.getElementsByClassName("yt-user-info").style.backgroundColor = "#B6B6B8";
 	var all = document.getElementsByClassName("summary");
 	for(var i = 0; i < all.length;i++)
 	{
